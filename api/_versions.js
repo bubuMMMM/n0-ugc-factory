7c952ee1949f4e6e4b1dc47e49ced1314a0997fb
bubuMMMM <135442447+bubuMMMM@users.noreply.github.com>
2026-09-20 18:47:22 +0200

    Centralize pipeline versions

---
+++ api/_versions.js
@@ -0,0 +1,6 @@
+module.exports={
+  VIDEO_INTELLIGENCE_VERSION:'video-intel-v7-face-geometry',
+  LAYOUT_VERSION:'face-first-v3',
+  HOOK_INTELLIGENCE_VERSION:'hook-intelligence-v4',
+  MANIFEST_VERSION:'video-catalog-v1'
+};
